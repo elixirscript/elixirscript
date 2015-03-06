f72d91e327436d240fada83b12fca7b41733146a
Bryan Joseph <bryanjos@gmail.com>
2015-03-05 18:34:48 -0600

    Added tests for map and if

---
+++ test/translator/map_test.exs
@@ -0,0 +1,25 @@
+defmodule ExToJS.Translator.Map.Test do
+  use ExUnit.Case
+  import ExToJS.TestHelper
+
+  test "translate empty map" do
+    ex_ast = quote do: %{}
+    js_code = "{}"
+
+    assert_translation(ex_ast, js_code)
+  end
+
+  test "translate map with elements" do
+    ex_ast = quote do: %{one: "one", two: "two"}
+    js_code = "{'one': 'one', 'two': 'two'}"
+
+    assert_translation(ex_ast, js_code)
+  end
+
+  test "translate map within map" do
+    ex_ast = quote do: %{one: "one", two: %{three: "three"}}
+    js_code = "{'one': 'one', 'two': {'three': 'three'}}"
+
+    assert_translation(ex_ast, js_code)
+  end
+end
