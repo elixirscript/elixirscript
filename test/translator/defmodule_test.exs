e7d4866e73240e2d21ff5a1e579c9313c1fb8388
Bryan Joseph <bryanjos@gmail.com>
2015-05-31 15:10:48 -0500

    Added new tests for inner modules

--- test/translator/defmodule_test.exs
+++ test/translator/defmodule_test.exs
@@ -76,4 +76,51 @@ defmodule ElixirScript.Translator.Defmodule.Test do
 
     assert_translation(ex_ast, js_code)
   end
+
+  should "translate modules with inner modules" do
+    ex_ast = quote do
+      defmodule Animals do
+        defmodule Elephant do
+          defstruct trunk: true
+        end
+
+
+        def something() do
+        end
+
+        defp something_else() do
+        end
+      end
+    end
+
+    js_code = """
+      const __MODULE__ = Atom('Animals');
+
+      const __MODULE__ = Atom('Elephant');
+      function defstruct(trunk = true) {
+         return {
+             __struct__: __MODULE__,
+             trunk: trunk
+         };
+      }
+      let Elephant = { defstruct: defstruct };
+
+      function something(){
+        return ul;
+      }
+
+      function something_else(){
+        return null;
+      }
+
+      let Animals = {
+        something: something,
+        Elephant: Elephant
+      };
+
+      export default Animals;
+    """
+
+    assert_translation(ex_ast, js_code)
+  end
 end
