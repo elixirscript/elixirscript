e075bec545bcc15ca25595680dd35b9051dca705
Bryan Joseph <bryanjos@gmail.com>
2015-12-23 21:21:17 -0600

    Removed implementations for enumerable and collectable

--- lib/elixir_script/universal/collectable.ex
+++ lib/elixir_script/universal/collectable.ex
@@ -1,13 +1,3 @@
 defprotocol ElixirScript.Collectable do
   def into(collectable)
 end
-
-defimpl ElixirScript.Collectable, for: List do
-  def into(original) do
-    {[], fn
-      list, {:cont, x} -> [ x | list ]
-      list, :done -> original ++ Elixir.Core.reverse(list)
-      _, :halt -> :ok
-    end}
-  end
-end
