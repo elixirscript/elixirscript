ca8b61db8054a8b477fe81d5243771d30b4405ee
Bryan Joseph <bryanjos@gmail.com>
2016-02-27 08:25:55 -0600

    Added @ to imported items from Kernel in ElixirScript.Kernel

--- lib/elixir_script/prelude/kernel.ex
+++ lib/elixir_script/prelude/kernel.ex
@@ -1,6 +1,6 @@
 defmodule ElixirScript.Kernel do
   import Kernel, only: [defmodule: 2, def: 1, def: 2, defp: 2,
-  defmacro: 1, defmacro: 2, defmacrop: 2, ||: 2, !: 1, ++: 2, in: 2, &&: 2, ===: 2]
+  defmacro: 1, defmacro: 2, defmacrop: 2, ||: 2, !: 1, ++: 2, in: 2, &&: 2, ===: 2, @: 1]
 
   defmacro if(condition, clauses) do
     build_if(condition, clauses)
