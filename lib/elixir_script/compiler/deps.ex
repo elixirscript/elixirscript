570c229f55351352c2bde10acac907218aecb8a7
Bryan Joseph <bryanjos@gmail.com>
2016-10-09 20:07:34 -0500

    Add function to deps module to find modules in files

--- lib/elixir_script/compiler/deps.ex
+++ lib/elixir_script/compiler/deps.ex
@@ -1,4 +1,5 @@
 defmodule ElixirScript.Compiler.Deps do
+  alias ElixirScript.Translator.Utils
 
   def get_deps_paths(env \\ Mix.env) do
     deps = Mix.Dep.loaded([env: "env"])
@@ -11,5 +12,61 @@ defmodule ElixirScript.Compiler.Deps do
     end)
   end
 
+  def get_module_filepath_map(env \\ Mix.env) do
+    deps_paths = get_deps_paths(env)
+
+    Enum.reduce(deps_paths, Map.new, fn({dep, paths}, map) ->
+
+      file_paths = Enum.flat_map(paths, fn(path) ->
+        path = Path.join(path, "**/*.{ex,exs,exjs}")
+        |> Path.wildcard
+      end)
+
+      file_paths = Enum.reduce(file_paths, Map.new, fn(path, map) ->
+        quoted = path
+        |> File.read!
+        |> Code.string_to_quoted!
+
+        { _, modules } = Macro.postwalk(quoted, %{protocols: [], modules: [], impls: []}, &get_defmodules(&1, &2))
+
+        { path, modules }
+        Map.put(map, path, modules)
+      end)
+
+      {dep, file_paths}
+      Map.put(map, dep, file_paths)
+    end)
+
+  end
+
+
+  defp get_defmodules({:defprotocol, _, [{:__aliases__, _, _} = the_alias, _]} = ast, state) do
+    state = Map.update!(state, :protocols, fn(l) -> [ Utils.quoted_to_name(the_alias) | l ] end)
+    { ast, state }
+  end
+
+  defp get_defmodules({:defprotocol, _, [{:__aliases__, _, _} = the_alias, _]} = ast, state) do
+    state = Map.update!(state, :protocols, fn(l) -> [ Utils.quoted_to_name(the_alias) | l ] end)
+    { ast, state }
+  end
+
+  defp get_defmodules({:defimpl, _, [ {:__aliases__, _, _} = the_alias, [for: type],  _ ]} = ast, state) do
+    state = Map.update!(state, :impls, fn(l) -> [  { Utils.quoted_to_name(the_alias), Utils.quoted_to_name(type) } | l ] end)
+    { ast, state }
+  end
+
+  defp get_defmodules({:defimpl, _, [ {:__aliases__, _, _} = the_alias, [for: type],  _ ]} = ast, state) do
+    state = Map.update!(state, :impls, fn(l) -> [  { Utils.quoted_to_name(the_alias), Utils.quoted_to_name(type) } | l ] end)
+    { ast, state }
+  end
+
+  defp get_defmodules({:defmodule, _, [{:__aliases__, _, _} = the_alias, [do: _]]} = ast, state) do
+    state = Map.update!(state, :modules, fn(l) -> [ Utils.quoted_to_name(the_alias) | l ] end)
+    { ast, state }
+  end
+
+  defp get_defmodules(ast, state) do
+    { ast, state }
+  end
 
 end
---
+++ test/compiler/deps_test.exs
@@ -0,0 +1,5 @@
+defmodule ElixirScript.Compiler.Deps.Test do
+  use ExUnit.Case
+  alias  ElixirScript.Compiler.Cache
+
+end
