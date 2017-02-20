94fdb1a7af832c49e398e013f00b66b9c0aa930b
Bryan Joseph <bryanjos@gmail.com>
2017-02-20 17:35:58 -0600

    Add support for defdelegate

--- lib/elixir_script/passes/find_functions.ex
+++ lib/elixir_script/passes/find_functions.ex
@@ -5,9 +5,17 @@ defmodule ElixirScript.Passes.FindFunctions do
   def execute(data, _) do
     new_data = Enum.map(data.data, fn { module_name, module_data } ->
 
-      %{def: functions, defp: private_functions, defgen: generators, defgenp: private_generators, defmacro: macros, defmacrop: private_macros } = get_functions_from_module(module_data.ast)
+      %{
+        def: functions, 
+        defp: private_functions, 
+        defgen: generators, 
+        defgenp: private_generators, 
+        defmacro: macros, 
+        defmacrop: private_macros,
+        defdelegate: delegates
+      } = get_functions_from_module(module_data.ast)
 
-      module_data = Map.put(module_data, :functions, functions ++ generators)
+      module_data = Map.put(module_data, :functions, functions ++ generators ++ delegates)
       |> Map.put(:private_functions, private_functions ++ private_generators)
       |> Map.put(:macros, macros)
       |> Map.put(:private_macros, private_macros)
@@ -21,40 +29,49 @@ defmodule ElixirScript.Passes.FindFunctions do
   defp get_functions_from_module({:__block__, _, list}) do
     Enum.reduce(list, new_function_map(), fn
       ({type, _, [{:when, _, [{name, _, params} | _guards] }, _] }, state) when type in @function_types and is_atom(params) ->
-      arity = 0
-
-      add_function_to_map(state, type, name, arity)
+        arity = 0
+        add_function_to_map(state, type, name, arity)
 
       ({type, _, [{:when, _, [{name, _, params} | _guards] }, _] }, state) when type in @function_types ->
-      arity = if is_nil(params), do: 0, else: length(params)
-
-      add_function_to_map(state, type, name, arity)
+        arity = if is_nil(params), do: 0, else: length(params)
+        add_function_to_map(state, type, name, arity)
 
       ({type, _, [{name, _, params}, _]}, state) when type in @function_types and is_atom(params) ->
-      arity = 0
-
-      add_function_to_map(state, type, name, arity)
+        arity = 0
+        add_function_to_map(state, type, name, arity)
 
       ({type, _, [{name, _, params}, _]}, state) when type in @function_types ->
-      arity = if is_nil(params), do: 0, else: length(params)
+        arity = if is_nil(params), do: 0, else: length(params)
         add_function_to_map(state, type, name, arity)
 
       ({type, _, [{name, _, params}]}, state) when is_atom(params) and type in @function_types ->
-      arity = 0
-      add_function_to_map(state, type, name, arity)
+        arity = 0
+        add_function_to_map(state, type, name, arity)
 
       ({type, _, [{name, _, params}]}, state) when type in @function_types ->
-      arity = length(params)
-      add_function_to_map(state, type, name, arity)
+        arity = length(params)
+        add_function_to_map(state, type, name, arity)
 
-    _, state ->
-      state
+      ({:defdelegate, _, [{name, _, params}], _}, state) ->
+        arity = length(params)
+        add_function_to_map(state, :defdelegate, name, arity)      
+
+      _, state ->
+        state
 
     end)
   end
 
   defp new_function_map() do
-    %{ def: Keyword.new, defp: Keyword.new, defgen: Keyword.new, defgenp: Keyword.new, defmacro: Keyword.new, defmacrop: Keyword.new }
+    %{  
+      def: Keyword.new, 
+      defp: Keyword.new, 
+      defgen: Keyword.new, 
+      defgenp: Keyword.new, 
+      defmacro: Keyword.new, 
+      defmacrop: Keyword.new,
+      defdelegate: Keyword.new
+    }
   end
 
   defp get_functions_from_module(_) do
---
+++ test/translator/defdelegate_test.exs
@@ -0,0 +1,28 @@
+defmodule ElixirScript.Translator.Defdelegate.Test do
+  use ExUnit.Case
+  import ElixirScript.TestHelper
+
+  test "translate defdelegate" do
+    ex_ast = quote do: defdelegate reverse(list), to: :lists
+
+    js_code = """
+     const reverse = function(list) {
+             return Elixir.Core.Functions.reverse(list);
+         };
+    """
+
+    assert_translation(ex_ast, js_code)
+  end
+
+  test "translate defdelegate as another name" do
+    ex_ast = quote do: defdelegate other_reverse(list), to: :lists, as: :reverse
+
+    js_code = """
+     const other_reverse = function(list) {
+             return Elixir.Core.Functions.reverse(list);
+         };
+    """
+
+    assert_translation(ex_ast, js_code)
+  end
+end
