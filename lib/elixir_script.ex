b313a69f833241d6ed9e3f32700a539edab5add7
Bryan Joseph <bryanjos@gmail.com>
2017-02-26 12:16:30 -0600

    Remove copy_bootstrap_to_destination

--- lib/elixir_script.ex
+++ lib/elixir_script.ex
@@ -209,19 +209,8 @@ defmodule ElixirScript do
   Returns the contents of the bootrstrap js file
   """
   def get_bootstrap_js(module_format) do
-    path = Path.join([operating_path, "build", to_string(module_format),"Elixir.Bootstrap.js"])
+    path = Path.join([operating_path, "build", to_string(module_format), "Elixir.Bootstrap.js"])
     File.read!(path)
-  end  
-
-  @doc """
-  Copies the javascript that makes up the ElixirScript bootstrap
-  to the specified location
-  """
-  def copy_bootstrap_to_destination(module_format, destination) do
-    path = Path.join([operating_path, "build", to_string(module_format),"Elixir.Bootstrap.js"])
-    base = Path.basename(path)
-    File.mkdir_p!(destination)
-    File.cp!(path, Path.join([destination, base]))
   end
 
   #Gets path to js files whether the mix project is available
