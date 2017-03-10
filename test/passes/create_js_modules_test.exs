33432ad193b9a04b73595332f5aa5bce977407cb
Stephan van Diepen <svdiepen@zilverline.com>
2017-03-10 09:11:21 +0100

    Add load spec

---
+++ test/passes/create_js_modules_test.exs
@@ -0,0 +1,20 @@
+defmodule ElixirScript.Passes.CreateJSModules.Test do
+  use ExUnit.Case
+
+  import ElixirScript.TestHelper
+
+  alias ElixirScript.Passes.CreateJSModules
+  alias ESTree.Tools.Generator
+
+  test "load" do
+    load_js = CreateJSModules.load
+    |> Generator.generate
+
+    expected_js =
+"Elixir.load = function(module) {
+    return module.__load(Elixir);
+}"
+
+    assert(load_js == expected_js)
+  end
+end
