8728ab99c53ef00bac16af8f38c83012ba6dbff7
Bryan Joseph <bryanjos@gmail.com>
2017-03-11 20:55:29 -0600

    Javascript formatting

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
-import nodeResolve from "rollup-plugin-node-resolve";
-import babel from "rollup-plugin-babel";
+import nodeResolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
 
 export default {
-  entry: "src/javascript/elixir.js",
-  moduleName: "Bootstrap",
+  entry: 'src/javascript/elixir.js',
+  moduleName: 'Bootstrap',
   plugins: [
     nodeResolve({ jsnext: true }),
     babel({
-      babelrc: false
-    })
+      babelrc: false,
+    }),
   ],
-  targets: [{ dest: "priv/build/iife/Elixir.Bootstrap.js", format: "iife" }]
+  targets: [{ dest: 'priv/build/iife/Elixir.Bootstrap.js', format: 'iife' }],
 };
