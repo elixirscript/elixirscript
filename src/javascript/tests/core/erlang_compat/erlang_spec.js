dc00d1df189347c23dfe3d79ac0b1397ad0a1c38
Bryan Joseph <bryanjos@gmail.com>
2017-08-11 16:19:22 -0500

    Make sure that is_atom considers true, false, and null as atoms

--- src/javascript/tests/core/erlang_compat/erlang_spec.js
+++ src/javascript/tests/core/erlang_compat/erlang_spec.js
@@ -1,12 +1,25 @@
 import test from 'ava';
 import Core from '../../../lib/core';
 
+test('is_atom', t => {
+  t.is(Core.erlang.is_atom(null), true);
+  t.is(Core.erlang.is_atom(true), true);
+  t.is(Core.erlang.is_atom(false), true);
+  t.is(Core.erlang.is_atom(Symbol.for('error')), true);
+  t.is(Core.erlang.is_atom('Hello'), false);
+});
+
 test('atom_to_binary', t => {
   t.is(Core.erlang.atom_to_binary(Symbol.for('error')), 'error');
   t.is(
     Core.erlang.atom_to_binary(Symbol.for('error'), Symbol.for('utf8')),
     'error'
   );
+
+  t.is(Core.erlang.atom_to_binary(null, Symbol.for('utf8')), 'nil');
+
+  t.is(Core.erlang.atom_to_binary(true, Symbol.for('utf8')), 'true');
+  t.is(Core.erlang.atom_to_binary(false, Symbol.for('utf8')), 'false');
 
   t.throws(
     () => Core.erlang.atom_to_binary(Symbol.for('error'), Symbol.for('utf16')),
@@ -23,7 +36,7 @@ test('list_concatenation', t => {
     3,
     4,
     5,
-    6,
+    6
   ]);
 });
 
