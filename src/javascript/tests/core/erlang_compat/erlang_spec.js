68afab56e4bf4c483a2f7690c7ebb4f529206df8
Bryan Joseph <bryanjos@gmail.com>
2017-08-13 11:25:40 -0500

    Add more erlang compat tests

--- src/javascript/tests/core/erlang_compat/erlang_spec.js
+++ src/javascript/tests/core/erlang_compat/erlang_spec.js
@@ -9,6 +9,54 @@ test('is_atom', t => {
   t.is(Core.erlang.is_atom('Hello'), false);
 });
 
+test('is_boolean', t => {
+  t.is(Core.erlang.is_boolean(null), false);
+  t.is(Core.erlang.is_boolean(true), true);
+  t.is(Core.erlang.is_boolean(false), true);
+  t.is(Core.erlang.is_boolean(Symbol.for('error')), false);
+  t.is(Core.erlang.is_boolean('Hello'), false);
+});
+
+test('is_number', t => {
+  t.is(Core.erlang.is_number(null), false);
+  t.is(Core.erlang.is_number(1), true);
+  t.is(Core.erlang.is_number(2.3), true);
+  t.is(Core.erlang.is_number(Symbol.for('error')), false);
+  t.is(Core.erlang.is_number('Hello'), false);
+});
+
+test('is_float', t => {
+  t.is(Core.erlang.is_float(null), false);
+  t.is(Core.erlang.is_float(1), false);
+  t.is(Core.erlang.is_float(2.3), true);
+  t.is(Core.erlang.is_float(Symbol.for('error')), false);
+  t.is(Core.erlang.is_float('Hello'), false);
+});
+
+test('is_integer', t => {
+  t.is(Core.erlang.is_integer(null), false);
+  t.is(Core.erlang.is_integer(1), true);
+  t.is(Core.erlang.is_integer(2.3), false);
+  t.is(Core.erlang.is_integer(Symbol.for('error')), false);
+  t.is(Core.erlang.is_integer('Hello'), false);
+});
+
+test('is_function', t => {
+  t.is(Core.erlang.is_function(null), false);
+  t.is(Core.erlang.is_function(Math.max), true);
+  t.is(Core.erlang.is_function(2.3), false);
+  t.is(Core.erlang.is_function(Symbol.for('error')), false);
+  t.is(Core.erlang.is_function(() => 'hello'), true);
+});
+
+test('is_list', t => {
+  t.is(Core.erlang.is_list(null), false);
+  t.is(Core.erlang.is_list([]), true);
+  t.is(Core.erlang.is_list(2.3), false);
+  t.is(Core.erlang.is_list(Symbol.for('error')), false);
+  t.is(Core.erlang.is_list(() => 'hello'), false);
+});
+
 test('atom_to_binary', t => {
   t.is(Core.erlang.atom_to_binary(Symbol.for('error')), 'error');
   t.is(
