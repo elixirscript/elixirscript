5bdbd78f41691ad94d741dc73caa00ae5652fbf3
vans163 <vans_163@yahoo.com>
2017-08-31 23:01:00 -0400

    Add map_to_object/2
    Squashed commit of the following:
    
    commit 2c2198e4595bba342fca82c31dbb4b016ebd49b6
    Author: vans163 <vans_163@yahoo.com>
    Date:   Wed Aug 30 13:00:58 2017 -0400
    
        export default
    
    commit 1400a8b943124623a518d33dea4b1acc75e7a5c1
    Merge: ce710b5 338e792
    Author: vans163 <vans_163@yahoo.com>
    Date:   Wed Aug 30 12:57:09 2017 -0400
    
        Merge branch 'map_to_valid_object' of https://github.com/vans163/elixirscript into map_to_valid_object
    
    commit ce710b56997fd456d05849ade5b1a395d9e214a7
    Author: vans163 <vans_163@yahoo.com>
    Date:   Wed Aug 30 12:54:37 2017 -0400
    
        fix whitespace
    
    commit 3833893173fb400b3e558f1f296a846a0b4f7d0e
    Author: vans163 <vans_163@yahoo.com>
    Date:   Wed Aug 30 12:44:54 2017 -0400
    
        changed to use map_to_object/2
    
    commit a7ec7029d5d50497bd8f4a7652f7dcc7fc29fa38
    Author: vans163 <vans_163@yahoo.com>
    Date:   Fri Aug 25 13:29:38 2017 -0400
    
        fix broken syntax on js_test.ex
    
    commit 0b95bc11078d8de69d10edbeabaa295e8fe90ede
    Author: vans163 <vans_163@yahoo.com>
    Date:   Fri Aug 25 13:24:01 2017 -0400
    
        add map_to_valid_object/1, symbol_to_string/1
    
    commit 338e792f22bf845c2b4870a6a3ad9f6a06b97b05
    Author: vans163 <vans_163@yahoo.com>
    Date:   Wed Aug 30 12:44:54 2017 -0400
    
        changed to use map_to_object/2
    
    commit e1cc332350d454c710051cb018e35fc7e32242a7
    Author: vans163 <vans_163@yahoo.com>
    Date:   Fri Aug 25 13:29:38 2017 -0400
    
        fix broken syntax on js_test.ex
    
    commit 8e8e4da3ddcdadabccece5ea158ef1aaaf9c0bf0
    Author: vans163 <vans_163@yahoo.com>
    Date:   Fri Aug 25 13:24:01 2017 -0400
    
        add map_to_valid_object/1, symbol_to_string/1

--- src/javascript/tests/core/functions.spec.js
+++ src/javascript/tests/core/functions.spec.js
@@ -24,3 +24,16 @@ test('split_at', t => {
   t.deepEqual(Functions.split_at('abc', -1000).values, ['', 'abc']);
   t.deepEqual(Functions.split_at('😀abélkm', 4).values, ['😀abé', 'lkm']);
 });
+
+test('map_to_object/2', t => {
+  const map = new Map([
+    [Symbol.for('key'), "value"],
+    [Symbol.for('anotherKey'), "value2"],
+  ]);
+
+  const options = [new Core.Tuple(Symbol.for('keys'),Symbol.for('strings'))];
+
+  const result = Functions.map_to_object(map, options);
+
+  t.deepEqual(result, {key: "value", anotherKey: "value"});
+});
