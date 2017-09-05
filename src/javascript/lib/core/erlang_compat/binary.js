01bdf02b9bc45ce5c81a1a14d16727f6536abda8
vans163 <vans_163@yahoo.com>
2017-09-05 16:01:56 -0400

    added more :erlang.binary functions

--- src/javascript/lib/core/erlang_compat/binary.js
+++ src/javascript/lib/core/erlang_compat/binary.js
@@ -1,14 +1,65 @@
 import erlang from './erlang';
+import proplists from './proplists';
+
+function at(subject, pos) {
+  return subject.charAt(pos);
+}
 
 function copy(subject, n = 1) {
   return subject.repeat(n);
+}
+
+function first(subject) {
+  //TODO: raise badarg on size 0
+  return at(subject, 0);
+}
+
+function last(subject) {
+  //TODO: raise badarg on size 0
+  return subject.slice(-1);
 }
 
 function list_to_bin(bytelist) {
   return erlang.list_to_binary(bytelist);
 }
 
+//TODO: How to create a tuple on JS side from part/3?
+//function part(subject, posOrTuple, len=null) {
+//    if (len == null) "/2 called" else "/3 called"
+//}
+
+function part(subject, pos, len) {
+    return subject.substr(pos, len);
+}
+
+//TODO: Support more options
+//TODO: pattern cannot be list of strings
+function replace(subject, pattern, replacement, options=[]) {
+    const opt_global = proplists.get_value(Symbol('global'), options);
+
+    var regex;
+    if (opt_global.toString() != Symbol('undefined').toString()) {
+        regex = new RegExp(pattern, 'g');
+    } else {
+        regex = new RegExp(pattern, '');
+    }
+    
+    return subject.replace(regex, replacement)
+}
+
+//TODO: Support more options, global is implied
+//TODO: pattern cannot be list of strings
+function split(subject, pattern, options=[]) {
+    return subject.split(pattern)
+}
+
 export default {
+  at,
   copy,
+  first,
+  last,
   list_to_bin,
+  part,
+  replace,
+  split,
 };
--- src/javascript/tests/core/erlang_compat/binary_spec.js
+++ src/javascript/tests/core/erlang_compat/binary_spec.js
@@ -1,6 +1,11 @@
 import test from 'ava';
 import Core from '../../../lib/core';
 
+test('at', (t) => {
+  let result = Core.binary.at('abc', 0);
+  t.deepEqual(result, 'a');
+});
+
 test('copy', (t) => {
   let result = Core.binary.copy('h', 3);
   t.deepEqual(result, 'hhh');
@@ -9,7 +14,39 @@ test('copy', (t) => {
   t.deepEqual(result, 'h');
 });
 
+test('first', (t) => {
+  let result = Core.binary.first('abc');
+  t.deepEqual(result, 'a');
+});
+
+test('last', (t) => {
+  let result = Core.binary.last('abc');
+  t.deepEqual(result, 'c');
+});
+
 test('list_to_bin', (t) => {
   const result = Core.binary.list_to_bin([104, 101, 108, 108, 111]);
   t.deepEqual(result, 'hello');
+});
+
+test('part', (t) => {
+  let result = Core.binary.part('abcde', 1, 1);
+  t.deepEqual(result, 'b');
+
+  result = Core.binary.part('abcde', 1, 3);
+  t.deepEqual(result, 'bcd');
+});
+
+test('replace', (t) => {
+  let result = Core.binary.replace('abcb', 'b', 'c');
+  t.deepEqual(result, 'accb');
+
+  //TODO: How to make a proplist here?
+  //result = Core.binary.replace('abcb', 'b', 'c', [global: true]);
+  //t.deepEqual(result, 'accc');
+});
+
+test('split', (t) => {
+  let result = Core.binary.split('abcd', 'b');
+  t.deepEqual(result, ['a', 'cd']);
 });
