13844dd3aea68dc9fa7853e318a274bd2cc3ce25
Bryan Joseph <bryanjos@gmail.com>
2017-09-12 21:11:10 -0500

    Add map functions to check for equality of keys
    This is done because map key equality does not work for maps,
    arrays, tuples, or anything else not a number, string or symbol
    
    fixes #406

--- src/javascript/tests/core/erlang_compat/maps_spec.js
+++ src/javascript/tests/core/erlang_compat/maps_spec.js
@@ -13,10 +13,37 @@ test('find', (t) => {
   myMap = new Map([['t', 'b']]);
   result = Core.maps.find('t', myMap);
   t.deepEqual(result.values, [Symbol.for('ok'), 'b']);
+
+  myMap = new Map([[[1], 'b']]);
+  result = Core.maps.find([1], myMap);
+  t.deepEqual(result.values, [Symbol.for('ok'), 'b']);
+
+  myMap = new Map([[new Map(), 'b']]);
+  result = Core.maps.find(new Map(), myMap);
+  t.deepEqual(result.values, [Symbol.for('ok'), 'b']);
 });
 
 test('fold', (t) => {
   const myMap = new Map([['a', 1], ['b', 2]]);
   const result = Core.maps.fold((k, v, acc) => acc + v, 0, myMap);
   t.is(result, 3);
+});
+
+test('is_key', (t) => {
+  const myMap = new Map([['a', 1], ['b', 2]]);
+  let result = Core.maps.is_key('a', myMap);
+  t.is(result, true);
+
+  result = Core.maps.is_key('c', myMap);
+  t.is(result, false);
+});
+
+test('remove', (t) => {
+  let myMap = new Map([['a', 1], ['b', 2]]);
+  let result = Core.maps.remove('a', myMap);
+  t.is(result.has('a'), false);
+
+  myMap = new Map([[[1], 1], ['b', 2]]);
+  result = Core.maps.remove([1], myMap);
+  t.is(Core.maps.__has(result, [1]), false);
 });
