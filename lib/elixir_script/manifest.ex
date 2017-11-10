a0e5eb1e7c1ab8e14b46cee7928ac1ca74c044aa
Bryan Joseph <bryanjos@gmail.com>
2017-11-09 20:07:19 -0600

    Add output path for js_path

--- lib/elixir_script/manifest.ex
+++ lib/elixir_script/manifest.ex
@@ -7,15 +7,22 @@ defmodule ElixirScript.Manifest do
   end
 
   @spec write_manifest(binary, [{atom, map}], map) :: :ok
-  def write_manifest(manifest_path, modules, _opts) do
+  def write_manifest(manifest_path, modules, opts) do
+    output_path = if opts.output == nil or opts.output == :stdout do
+     ""
+    else
+      Path.dirname(opts.output)
+    end
+
     data = Enum.reduce(modules, %{}, fn {module, info}, current_data ->
       info = %{
         references: info.used_modules,
         last_modified: info.last_modified,
         beam_path: Map.get(info, :beam_path),
         source: Map.get(info, :file),
-        js_path: "Elixir.#{module}.js"
+        js_path: Path.join(output_path, "#{module}.js")
       }
+
       Map.put(current_data, module, info)
     end)
 
